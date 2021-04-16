930e6f19d02431383930c5bab2c9df0129580e3f
dangtdn <nguyendanglop93@gmail.com>
2021-04-16 15:21:55 +0700

    'update_profile_setUpPass_announcement'

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
-// function slidebar-btn
-document.querySelector('.slidebar-btn').addEventListener('click',() => {
-    document.querySelector('.wrapper').classList.toggle('collapse-slide');
-});
-
-// Zoom image
+// declare variables
 var modalImage = document.querySelector(".modal-box");
 var closeModal = document.querySelector(".btn-close-modal-box");
 var prevImage = document.querySelector(".modal-box-img-prev");
@@ -14,43 +9,55 @@ var ImageModal = null;
 var ImageShow = document.querySelector(".modal-box-img");
 var index = 0;
 var scrollTop = document.querySelector(".scroll-to-top");
-console.log(ImageContainModal);
 
-function updateMedia(index){
-    let ImageClone = ImageModal[index].cloneNode() || "";
-    ImageShow.innerHTML = "";
-    ImageShow.appendChild(ImageClone);
-}
+// function slidebar-btn
+document.querySelector('.slidebar-btn').addEventListener('click',() => {
+    document.querySelector('.wrapper').classList.toggle('collapse-slide');
+});
 
-ImageContainModal.forEach(function(el, idx){
-    el.addEventListener("click", function(){
-        modalImage.classList.add("active");
-
-        ImageModalSelect = this;
-        ImageModal = this.querySelectorAll(".post-media>*");
-        index = 0;
-
+// Zoom image
+if(closeModal !== null && 
+    prevImage !== null && 
+    nextImage !== null &&
+    ImageContainModal !== null &&
+    ImageShow !== null
+) {
+    function updateMedia(index){
+        let ImageClone = ImageModal[index].cloneNode() || "";
+        ImageShow.innerHTML = "";
+        ImageShow.appendChild(ImageClone);
+    }
+    
+    ImageContainModal.forEach(function(el, idx){
+        el.addEventListener("click", function(){
+            modalImage.classList.add("active");
+    
+            ImageModalSelect = this;
+            ImageModal = this.querySelectorAll(".post-media>*");
+            index = 0;
+    
+            updateMedia(index);
+            console.log("Clicked");
+        });    
+    });
+    
+    
+    closeModal.addEventListener("click", function(){
+        modalImage.classList.remove("active");
+    });
+    
+    prevImage.addEventListener("click", function(){
+        (index - 1 < 0) ? index = ImageModal.length - 1 : index = index-1 ;
+    
         updateMedia(index);
-        console.log("Clicked");
-    });    
-});
-
-
-closeModal.addEventListener("click", function(){
-    modalImage.classList.remove("active");
-});
-
-prevImage.addEventListener("click", function(){
-    (index - 1 < 0) ? index = ImageModal.length - 1 : index = index-1 ;
-
-    updateMedia(index);
-});
-
-nextImage.addEventListener("click", function(){
-    (index + 1 > ImageModal.length - 1) ? index = 0 : index = index+1 ;
-
-    updateMedia(index);
-});
+    });
+    
+    nextImage.addEventListener("click", function(){
+        (index + 1 > ImageModal.length - 1) ? index = 0 : index = index+1 ;
+    
+        updateMedia(index);
+    });
+}
 
 // Scroll Top
 window.addEventListener("scroll", () => {
@@ -59,6 +66,7 @@ window.addEventListener("scroll", () => {
     }else {
         scrollTop.classList.remove("active");
     }
+    console.log('run')
 });
 
 scrollTop.addEventListener("click", () => {
